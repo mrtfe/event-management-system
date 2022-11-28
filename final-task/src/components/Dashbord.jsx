99292c7f0fcc3e57daefd2b7c4f61f71b1e96021
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-11-28 21:04:59 +0200

    add attendee inplemented

--- final-task/src/components/Dashbord.jsx
+++ final-task/src/components/Dashbord.jsx
@@ -1,18 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/dashbord.css";
 
 export function Dashbord() {
+  const [attendees, setAttendees] = useState([]);
+  const [attendee, setAttendee] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    age: "",
+  });
+
+  const randomIdGenerator = () => {
+    return Math.floor(Math.random() * 100000);
+  };
+
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    const inputData = e.target.value;
+    setAttendee({ ...attendee, [inputName]: inputData });
+    console.log(attendee);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = randomIdGenerator();
+    setAttendees([...attendees, { attendee, id }]);
+    console.log(attendees);
+  };
   return (
     <div className="dashbord-wrapper">
       <div className="dashbord-header header">Management dashbord</div>
       <div className="form-container">
         <div className="form-header header">Create Attendee</div>
-        <form action="submit">
-          <input type="text" placeholder="First name" required />
-          <input type="email" placeholder="Email" required />
-          <input type="text" placeholder="Last name" required />
-          <input type="number" placeholder="Age" required />
-          <button className="submit-btn">Submit</button>
+        <form action="submit" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="First name"
+            required
+            name="firstName"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Last name"
+            required
+            name="email"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            name="lastName"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Age"
+            required
+            name="age"
+            onChange={handleChange}
+          />
+          <button type="submit" className="submit-btn">
+            Submit
+          </button>
         </form>
       </div>
       <div className="attendees-container">
@@ -24,6 +76,15 @@ export function Dashbord() {
             <div className="header-column">Email</div>
             <div className="header-column">Age</div>
           </div>
+          {attendees.length > 0 &&
+            attendees.map((att) => (
+              <div className="attendee-row" key={att.id}>
+                <div className="attendee-col">{att.attendee.firstName}</div>
+                <div className="attendee-col">{att.attendee.lastName}</div>
+                <div className="attendee-col">{att.attendee.email}</div>
+                <div className="attendee-col">{att.attendee.age}</div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
