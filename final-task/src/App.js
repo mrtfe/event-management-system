50f2190f2e612fa154ad419f373c9baf508da611
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-11-29 20:49:32 +0200

    simple admin login implemented

--- final-task/src/App.js
+++ final-task/src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
 import { LoginForm } from "./components/LoginForm";
 import { Dashbord } from "./components/Dashbord";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+  });
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [error, setError] = useState(false);
+
   return (
     <div className="App">
-      <LoginForm />
-      <Dashbord />
+      {!loggedIn && (
+        <LoginForm
+          user={user}
+          setUser={setUser}
+          loggedIn={loggedIn}
+          setLoggedIn={setLoggedIn}
+          error={error}
+          setError={setError}
+        />
+      )}
+
+      {loggedIn && <Dashbord />}
     </div>
   );
 }
--- final-task/src/components/LoginForm.jsx
+++ final-task/src/components/LoginForm.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import "../styles/loginForm.css";
 
-export function LoginForm() {
+export function LoginForm(props) {
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    const inputData = e.target.value;
+    props.setUser({ ...props.user, [inputName]: inputData });
+  };
+
+  const handleSubmit = (e) => {
+    if (props.user.userName === "admin" && props.user.password === "cafe") {
+      props.setLoggedIn(true);
+      props.setError(false);
+    } else {
+      props.setError(true);
+      props.setLoggedIn(false);
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-header">Welcome to event management system</div>
       <form action="submit" className="login-form">
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <p>Incorrect username or password</p>
-        <button className="login-btn" type="button">
+        <input
+          type="text"
+          placeholder="username"
+          name="userName"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        {props.error && <p>Incorrect username or password</p>}
+        <button className="login-btn" type="button" onClick={handleSubmit}>
           Log in
         </button>
       </form>
