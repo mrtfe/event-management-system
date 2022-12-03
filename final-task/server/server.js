ec2f8a3ea49cf879d9468581ba275e67c0828ed2
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-12-03 19:59:53 +0200

    server side cleanup

--- final-task/server/server.js
+++ final-task/server/server.js
@@ -13,33 +13,36 @@ const adminsPath = "/api/admins";
 
 const port = 5000;
 
-app.get("/api/admins", (req, res) => {
-  const admins = readJsonFileSync("./admins.json");
-  res.json(admins);
-});
-
+// function that reads data from local json file
 function readJsonFileSync(filepath) {
   const file = fs.readFileSync(filepath);
   return JSON.parse(file);
 }
 
+// function that returns admins
+app.get("/api/admins", (req, res) => {
+  const admins = readJsonFileSync("./admins.json");
+  res.json(admins);
+});
+
+// function that returns attendees
 app.get(attendeesPath, (req, res) => {
   const attendees = readJsonFileSync("./attendees.json");
   res.json(attendees);
 });
 
-const randomIdGenerator = () => {
-  return Math.floor(Math.random() * 100000);
-};
-
+// function that adds new attendee
 function addAttendee(newAttendee) {
   const attendees = readJsonFileSync("./attendees.json", "utf8");
   attendees.push(newAttendee);
   fs.writeFileSync("./attendees.json", JSON.stringify(attendees, null));
 }
 
+const randomIdGenerator = () => {
+  return Math.floor(Math.random() * 100000);
+};
+
 app.post(attendeesPath, function (req, res) {
-  console.log(req.body.firstName);
   const newAttendee = {
     id: randomIdGenerator(),
     firstName: req.body.firstName,
@@ -48,10 +51,11 @@ app.post(attendeesPath, function (req, res) {
     age: req.body.age,
   };
   addAttendee(newAttendee);
-  console.log(newAttendee);
   res.send(newAttendee);
 });
+//
 
+// function that adds new admin
 function addAdmin(newAdmin) {
   const admins = readJsonFileSync("./admins.json", "utf8");
   admins.push(newAdmin);
@@ -67,28 +71,26 @@ app.post(adminsPath, function (req, res) {
   addAdmin(newUser);
   res.send("new admin registered");
 });
+//
 
+// function that removes attendee
 function removeAttendee(id) {
   const attendees = readJsonFileSync("./attendees.json", "utf8");
   const result = attendees.filter((item) => item.id !== parseInt(id));
-  console.log(id);
   fs.writeFileSync("./attendees.json", JSON.stringify(result, null));
 }
-
 app.delete(`${attendeesPath}/:id`, (req, res) => {
   removeAttendee(req.params.id);
   res.send();
 });
+//
 
+// function that edits attendee
 function editAttendee(editedAttende) {
   const attendees = readJsonFileSync("./attendees.json", "utf8");
-
-  console.log(attendees);
   const newAttendeesArray = attendees.map((attendee) =>
     attendee.id === editedAttende.id ? editedAttende : attendee
   );
-  console.log(newAttendeesArray);
-
   fs.writeFileSync("./attendees.json", JSON.stringify(newAttendeesArray));
 }
 
@@ -101,9 +103,7 @@ app.put(`${attendeesPath}/:id`, (req, res) => {
     age: req.body.age,
   };
   editAttendee(editedAttende);
-
-  console.log(req.params.id);
   res.send(editedAttende);
 });
-
+//
 app.listen(port, () => console.log(`server started on port: ${port} `));
